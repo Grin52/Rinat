dd73d58d67fc2f332ca1c4629861c0a31316f884
Grin52 <rinat.gumarov@mail.ru>
2025-01-10 19:19:29 +0500

    new commit

--- obuch.c
+++ obuch.c
@@ -1,16 +1,19 @@
-#include <stdio.h>>
-
-int main (void);
-{
-    short a = -5;
-    int b = 10;
-    float c = 5.4f;
-    double d = -6.5;
+#include <stdio.h>
  
-    int res_1 = a + b;
-    short res_2 = 100 - b;
-    float res_3 = 5.4 - c;
-    double res_4 = d * 4;
+int main(void)
+{
+    double height, a;
+    
+    printf("Enter the height and length of the triangle's base: ");
+    int res = scanf("%lf %lf", &height, &a);
+ 
+    if(res != 2) {      // проверка, что res не равна двум
+        printf("Data entry error\n");
+        return 0;       // завершение функции main и программы
+    }
+ 
+    double sq = height * a / 2.0;
+    printf("The square of the triangle is: %.2f", sq);
  
     return 0;
 }
