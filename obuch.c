e8140d16d3cbda98e8c77ad8339853315a29ea90
Grin52 <rinat.gumarov@mail.ru>
2025-01-10 18:32:41 +0500

    new commit

---
+++ obuch.c
@@ -0,0 +1,16 @@
+#include <stdio.h>>
+
+int main (void);
+{
+    short a = -5;
+    int b = 10;
+    float c = 5.4f;
+    double d = -6.5;
+ 
+    int res_1 = a + b;
+    short res_2 = 100 - b;
+    float res_3 = 5.4 - c;
+    double res_4 = d * 4;
+ 
+    return 0;
+}
