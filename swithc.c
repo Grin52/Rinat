02c24ec62dd5ecc51a2fd3862719e8e79289d98e
Grin52 <rinat.gumarov@mail.ru>
2025-01-24 17:18:28 +0500

    yes yes

---
+++ swithc.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char symbol;
+    if(scanf("%c", &symbol) != 1) {
+        printf("Input error.");
+        return 0;
+    }
+
+    switch (symbol) {
+         case 'a':
+            symbol = 'A';
+            
+            break;
+        case 'b':
+            symbol = 'B';
+            break;
+        case 'c':
+            symbol = 'C';
+            
+            break;
+        case 'd':
+            symbol = 'D';
+            
+            break;
+        case 'e':
+            symbol = 'E';
+            
+            break;
+        case 'f':
+            symbol = 'F';
+            
+            break;
+    }
+                  
+    printf ("%c\n", symbol);// здесь продолжайте программу
+
+   // __ASSERT_TESTS__ // макроопределение для тестирования (не убирать и должно идти непосредственно перед return 0)
+    return 0;
+}
