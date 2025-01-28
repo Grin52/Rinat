28ee450b7fc9d171086a8f4557bb5f49a0fbfa99
Grin52 <rinat.gumarov@mail.ru>
2025-01-29 00:30:08 +0500

    уикл 2 заработал

--- 9_cicle/cicle_2.c
+++ 9_cicle/cicle_2.c
@@ -9,10 +9,9 @@ int main(void)
     while (n != 0)
     {
         scanf("%d ", &n);
-    
+        if ( n != 0 )    
         printf ("%d ", n * n);
-    }
 
-    return 0;
-    
+    }
+    return 0;    
 }
